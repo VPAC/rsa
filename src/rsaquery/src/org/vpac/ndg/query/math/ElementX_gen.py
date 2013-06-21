ad9cf1cca5e5b1264b7670c994e36ff070b94326
Alex Fraser <adfries@vpac.org>
2013-06-21 14:32:30 +1000

    Added copyright and licence headers.

--- src/rsaquery/src/org/vpac/ndg/query/math/ElementX_gen.py
+++ src/rsaquery/src/org/vpac/ndg/query/math/ElementX_gen.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 
 #
+# This file is part of the Raster Storage Archive (RSA).
+#
+# The RSA is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# The RSA is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# the RSA.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2013 CRCSI - Cooperative Research Centre for Spatial Information
+# http://www.crcsi.com.au/
+#
+
+#
 # This program generates the scalar Element classes.
 #
 
@@ -10,7 +29,27 @@ import Element_types
 
 # Code
 
-CLASS_HEADER_TEMPLATE = Template("""
+CLASS_HEADER_TEMPLATE = Template("""/*
+ * This file is part of the Raster Storage Archive (RSA).
+ *
+ * The RSA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * The RSA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * the RSA.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2013 CRCSI - Cooperative Research Centre for Spatial Information
+ * http://www.crcsi.com.au/
+ */
+
+// THIS IS GENERATED CODE. Do not modify this file. See ElementX_gen.py.
+
 package org.vpac.ndg.query.math;
 
 /**
@@ -20,7 +59,6 @@ package org.vpac.ndg.query.math;
  *
  * @author Alex Fraser
  */
-// THIS IS GENERATED CODE. Do not modify this file. See ElementX_gen.py.
 public class $cname implements ScalarElement {
 	private $ptype value;
 	private boolean valid;
